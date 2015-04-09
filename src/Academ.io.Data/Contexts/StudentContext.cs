b11b28fd51b267c82328771c528eccd292a0de41
iGilga <pyua87@gmail.com>
2015-04-09 12:07:30 +0300

    config connectstring and add studentContex

---
+++ src/Academ.io.Data/Contexts/StudentContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using Academ.io.Models;
+
+namespace Academ.io.Data.Contexts
+{
+    public class StudentContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
