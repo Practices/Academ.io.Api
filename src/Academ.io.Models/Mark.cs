844c7f3fee07b4b809928ed8d5efba110628f20c
iGilga <pyua87@gmail.com>
2015-04-11 00:40:33 +0300

    Add mark model

---
+++ src/Academ.io.Models/Mark.cs
@@ -0,0 +1,16 @@
+﻿namespace Academ.io.Models
+{
+    public class Mark
+    {
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public TestType TestType { get; set; }
+    }
+
+    public class TestType   
+    {
+        public int TestTypeId { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
