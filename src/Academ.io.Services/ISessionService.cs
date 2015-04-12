cc733738fbff39e24668e57b696ebf54c805c4eb
iGilga <pyua87@gmail.com>
2015-04-13 00:43:11 +0300

    add serivce session

---
+++ src/Academ.io.Services/ISessionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Academ.io.Models;
+
+namespace Academ.io.Services
+{
+    public interface ISessionService
+    {
+        IEnumerable<Session> GetSessions(Guid studentId);
+    }
+}
---
+++ src/Academ.io.Services/SessionService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Academ.io.Data.Repositories;
+using Academ.io.Models;
+using Academ.io.University.Api.Services.Sessions;
+
+namespace Academ.io.Services
+{
+    public class SessionService: ISessionService
+    {
+        private readonly ISessionServiceApi sessionServiceApi;
+        private readonly IMarkRepository markRepository;
+
+        public SessionService(ISessionServiceApi sessionServiceApi,IMarkRepository markRepository)
+        {
+            this.sessionServiceApi = sessionServiceApi;
+            this.markRepository = markRepository;
+        }
+
+        public IEnumerable<Session> GetSessions(Guid studentId)
+        {
+            var sessions = this.sessionServiceApi.GetSessionsByStudent(studentId);
+
+            var marks = markRepository.GetMarks();
+
+
+            return null;
+        }
+    }
+}
