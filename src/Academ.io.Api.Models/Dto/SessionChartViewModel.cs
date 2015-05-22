7e60b3044cc1707bf2772499a6bebb1853c1f2d2
iGilga <pyua87@gmail.com>
2015-05-23 00:13:52 +0300

    add SessionChart

---
+++ src/Academ.io.Api.Models/Dto/SessionChartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Academ.io.Api.Models.Dto
+{
+    public class SessionChartViewModel
+    {
+        public string Name { get; set; }
+        public int TestId { get; set; }
+        public int PositiveCount { get; set; }
+        public int NegativeCount { get; set; }
+    }
+}
